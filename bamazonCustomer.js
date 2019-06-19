60aaa37dba14fe96610478b39237d139c7cea668
Mintewab <demissiewminte123@gmail.com>
2019-06-18 23:07:28 -0700

    add images file, saleItems.PNG

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,21 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table3');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+    password: "Alexwork12345%",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if(err) throw err;
+    console.log("connection ID" + connection.threadId);
+    productItem();
+})
+
+
+
